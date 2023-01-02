57ab574118a7f7b229633486a82ed835a25c5249
baksohyeon <bori12370@gmail.com>
2023-01-02 20:51:41 +0900

    refactor: 파일명 변경 및 로직 수정

--- src/util/time-transform.util.ts
+++
@@ -1,3 +0,0 @@
-export const secondToMilisecond = (seconds: number) => seconds * 1000;
-
-export const dayToMilisecond = (days: number) => days * 3600 * 24 * 1000;
---
+++ src/util/units-of-time-conversion.util.ts
@@ -0,0 +1,3 @@
+export const minuteToMilisecond = (minutes: number) => minutes * 60 * 1000;
+
+export const dayToMilisecond = (days: number) => days * 3600 * 24 * 1000;
