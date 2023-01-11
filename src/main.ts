e252f4de1b952e59e1f05db5504af38bb87c8adf
baksohyeon <bori12370@gmail.com>
2023-01-11 13:05:40 +0900

    chore: redis, hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,5 +14,10 @@ async function bootstrap() {
   });
   app.use(cookieParser());
   await app.listen(3001);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
+
 bootstrap();
