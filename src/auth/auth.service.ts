ad0de8bcb5a066a59b23e2cc572eaa3e743d6d80
baksohyeon <bori12370@gmail.com>
2023-01-11 15:30:22 +0900

    //WIP: 기존 토큰 생성 로직 모두 삭제
    및 캐시 매니저 DI

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {
   CACHE_MANAGER,
+  HttpStatus,
   Inject,
   Injectable,
   Logger,
@@ -19,16 +20,22 @@ import { Repository } from 'typeorm';
 import { LoginRequestUserDto } from './dto/login-request.dto';
 import ms from 'ms';
 import { v4 as uuidv4 } from 'uuid';
-import { AccessTokenUserPayload } from 'src/types/access-token-user-payload.interface';
 import { FreshTokens } from 'src/types/fresh-tokens.interface';
-import { AccessTokenPayload } from 'src/types/type';
+import {
+  AccessTokenPayload,
+  AccessTokenUserPayload,
+  RefreshTokenPayload,
+} from 'src/types/type';
 import { Cache } from 'cache-manager';
+import { json } from 'stream/consumers';
+import { redisPayload } from 'src/types/redis.type';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {}
@@ -92,139 +99,5 @@ export class AuthService {
       accessToken,
       refreshToken,
     };
-  }
-
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  private getTokenSignOptions(tokenType: 'access' | 'refresh'): JwtSignOptions {
-    if (tokenType === 'access') {
-      const accessJwtSignOptions: JwtSignOptions = {
-        secret: this.configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
-        expiresIn: ms(
-          this.configService.get<number>('JWT_ACCESS_TOKEN_EXPIRES_IN'),
-        ),
-      };
-      return accessJwtSignOptions;
-    }
-    if (tokenType === 'refresh') {
-      const jti = uuidv4();
-      const refreshJwtSignOptions: JwtSignOptions = {
-        secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
-        expiresIn: ms(
-          this.configService.get<number>('JWT_REFRESH_TOKEN_EXPIRES_IN'),
-        ),
-        issuer: 'dorito',
-        audience: [this.configService.get<string>('FRONTEND_URL')],
-        jwtid: jti,
-      };
-      return refreshJwtSignOptions;
-    }
-  }
-
-  private getCookieOptions(
-    tokenType: 'access' | 'refresh' | 'reset',
-  ): CookieOptions {
-    if (tokenType === 'access') {
-      const accessCookieOptions: CookieOptions = {
-        maxAge: ms(
-          this.configService.get<string>('JWT_ACCESS_TOKEN_EXPIRES_IN'),
-        ),
-        sameSite: 'lax',
-        secure: false,
-        httpOnly: false,
-      };
-      return accessCookieOptions;
-    }
-    if ((tokenType = 'refresh')) {
-      const refreshCookieOptions: CookieOptions = {
-        maxAge: ms(
-          this.configService.get<string>('JWT_REFRESH_TOKEN_EXPIRES_IN'),
-        ),
-        sameSite: true,
-        secure: false,
-      };
-      return refreshCookieOptions;
-    }
-    if ((tokenType = 'reset')) {
-      const resetCookieOptions: CookieOptions = {
-        maxAge: 0,
-        sameSite: true,
-        secure: false,
-      };
-      return resetCookieOptions;
-    }
-  }
-
-  async getAccessTokenCookieConfig(user: User): Promise<AccessCookieConfig> {
-    const payload = {
-      sub: user.id,
-      email: user.email,
-    };
-    const accessTokenOptions = this.getTokenSignOptions('access');
-    const accessToken = this.jwtService.sign(payload, accessTokenOptions);
-    const accessCookieOptions: CookieOptions = this.getCookieOptions('access');
-    return {
-      accessToken: accessToken,
-      accessCookieOptions,
-    } as AccessCookieConfig;
-  }
-
-  async getRefreshTokenCookieConfig(user: User): Promise<RefreshCookieConfig> {
-    const payload = {
-      sub: user.id,
-      tokenType: 'refresh',
-    };
-
-    const refreshTokenOptions: JwtSignOptions =
-      this.getTokenSignOptions('refresh');
-    const refreshToken = this.jwtService.sign(payload, refreshTokenOptions);
-    const refreshCookieOptions: CookieOptions =
-      this.getCookieOptions('refresh');
-
-    return {
-      refreshToken: refreshToken,
-      refreshCookieOptions,
-    } as RefreshCookieConfig;
-  }
-
-  resetCookieOptions(): CookieOptions {
-    return this.getCookieOptions('reset');
-  }
-
-  async saveHashedRefreshToken(
-    refreshToken: string,
-    id: string,
-  ): Promise<void> {
-    const hashedRefreshToken = await hash(refreshToken, 10);
-    await this.userRepository
-      .createQueryBuilder()
-      .update(User)
-      .set({ hashedRefreshToken: hashedRefreshToken })
-      .where('id = :id', { id })
-      .execute();
-  }
-
-  async checkRefreshGetUser(refreshToken: string, id: string) {
-    // TODO: 에러 핸들링
-    const user = await this.userRepository.findOneBy({ id });
-    const isValidRefreshToken = await compare(
-      refreshToken,
-      user.hashedRefreshToken,
-    );
-    if (!isValidRefreshToken) {
-      throw new UnauthorizedException('Invalid Refresh Token ');
-    }
-    return user;
-  }
-
-  async resetRefreshToken(id: string): Promise<void> {
-    //  유저가 로그아웃 할 때 사용
-    // TODO: 에러 핸들링
-    await this.userRepository
-      .createQueryBuilder()
-      .update(User)
-      .set({ hashedRefreshToken: null })
-      .where('id = :id', { id })
-      .execute();
   }
 }
