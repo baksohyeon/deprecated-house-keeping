8b17df4cffcf66f3c2db7c81eb7f27f8fcb0c3e3
baksohyeon <bori12370@gmail.com>
2023-01-24 21:25:58 +0900

    refactor: 필요없는  import 삭제

--- src/auth/test/auth.service.spec.ts
+++ src/auth/test/auth.service.spec.ts
@@ -1,12 +1,9 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { UserModule } from 'src/user/user.module';
-import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 import { AuthService } from '../auth.service';
-import { minuteToMilisecond } from 'src/util/units-of-time-conversion.util';
 import { User } from 'src/entities/user.entity';
 import { mockedConfigService } from './mocks/config.service.mock';
 import { mockedJwtService } from './mocks/jwt.service.mock';
@@ -47,27 +44,5 @@ describe('AuthService', () => {
     mockedUserRepository = module.get<Repository<User>>(
       getRepositoryToken(User),
     );
-  });
-
-  describe('When reset cookie options', () => {
-    it("should be return cookie's max age value is 0", () => {
-      const resetCookieOptions = authService.resetCookieOptions();
-      expect(resetCookieOptions.maxAge).toEqual(0);
-      expect(resetCookieOptions.sameSite).toBeTruthy();
-      expect(resetCookieOptions.secure).toBeFalsy();
-    });
-
-    it('reset null refrech token in database', () => {
-      const userId = 'uuid';
-      authService.resetRefreshToken(userId);
-
-      expect(mockedUserRepository.createQueryBuilder).toHaveBeenCalled();
-      expect(
-        mockedUserRepository.createQueryBuilder().update().set,
-      ).toHaveBeenCalledWith({ hashedRefreshToken: null });
-      expect(mockedUserRepository.createQueryBuilder().execute).toBeCalledTimes(
-        1,
-      );
-    });
   });
 });
