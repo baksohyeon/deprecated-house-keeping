4550b3ebae849565432af29f32a8a48ffe4f632b
baksohyeon <bori12370@gmail.com>
2023-01-25 20:27:54 +0900

    fix: commit 때 테스트 코드 돌리게 수정

--- src/auth/test/auth.controller.spec.ts
+++ src/auth/test/auth.controller.spec.ts
@@ -1,20 +1,51 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from '../auth.controller';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthService } from '../auth.service';
+import { User } from 'src/entities/user.entity';
+import { mockedConfigService } from './mocks/config.service.mock';
+import { mockedJwtService } from './mocks/jwt.service.mock';
+import { mockRepository } from './mocks/reposiotry.mock';
+import tokenConfig from 'src/config/token.config';
+import { mockTokenConfig } from './mocks/token.config.mock';
+import { RedisService } from '../redis/redis.service';
 
-describe('AuthController', () => {
-  let controller: AuthController;
+const mockedUser = {
+  id: 'uuid',
+  email: 'test@abc.com',
+  username: 'dorito',
+  refreshToken: null,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+} as User;
 
+describe('AuthService', () => {
+  let authService: AuthService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: tokenConfig.KEY,
+          useValue: mockTokenConfig,
+        },
+        {
+          provide: RedisService,
+          useValue: {},
+        },
+        {
+          provide: JwtService,
+          useValue: mockedJwtService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  // 얘한테 테스트 코드 허스키 붙이면 됨
-  // 쓴 코드 허스키한테 싹다 세팅해보기
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
