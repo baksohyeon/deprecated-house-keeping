d3224792765a1d101e393e91611212d06ee5b89a
baksohyeon <bori12370@gmail.com>
2023-02-06 14:04:44 +0900

    feat: user profile 라우팅 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
+import { RequestUser } from 'src/decorator/request-user.decorator';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @UseGuards(AuthGuard('jwt-access'))
+  @Get('/profile')
+  async getUserProfile(@RequestUser() userId: string): Promise<User> {
+    return this.userService.findUserById(userId);
+  }
+}
