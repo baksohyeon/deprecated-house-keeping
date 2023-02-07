9b6906bfc3abfd782d1f65b43e6e04e8c85d6b62
baksohyeon <bori12370@gmail.com>
2023-02-07 21:24:59 +0900

    feat: house 생성하는 메서드 추가

--- src/house/house.controller.ts
+++ src/house/house.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RequestUser } from 'src/decorator/request-user.decorator';
+import { HouseService } from './house.service';
 
 @Controller('house')
-export class HouseController {}
+export class HouseController {
+  constructor(private readonly houseService: HouseService) {}
+
+  @Post('/create')
+  async createHouse(@Body() houseName: string, @RequestUser() userId: string) {
+    return await this.houseService.createNewHouse('House name', userId);
+  }
+}
--- src/house/house.service.ts
+++ src/house/house.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { House } from 'src/entities/house.entity';
+import { HouseMember } from 'src/entities/houseMember.entity';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class HouseService {}
+export class HouseService {
+  constructor(
+    @InjectRepository(House)
+    private readonly houseRepository: Repository<House>,
+    @InjectRepository(HouseMember)
+    private readonly houseMemberRepository: Repository<HouseMember>,
+  ) {}
+
+  async createNewHouse(houseName: string, userId: string) {
+    const houseEntity = this.houseRepository.create({ name: houseName });
+    const house = await this.houseRepository.save(houseEntity);
+
+    const houseMemberEntity = new HouseMember();
+    Object.assign(houseMemberEntity, {
+      houseId: house.id,
+      userId,
+      role: 'Admin',
+      backlog: 'No Tasks',
+    });
+    const houseMember = await this.houseMemberRepository.save(
+      houseMemberEntity,
+    );
+    return houseMember;
+  }
+}
