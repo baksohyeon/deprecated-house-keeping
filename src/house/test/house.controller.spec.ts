22cc5bb96b9c18ce89b8683c21324061ca4c604c
baksohyeon <bori12370@gmail.com>
2023-02-08 17:04:54 +0900

    test: house 의존성 객체 주입 세팅

--- src/house/test/house.controller.spec.ts
+++ src/house/test/house.controller.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HouseController } from '../house.controller';
+import { HouseService } from '../house.service';
 
 describe('HouseController', () => {
   let controller: HouseController;
+  let service: HouseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HouseController],
+      providers: [
+        {
+          provide: HouseService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<HouseController>(HouseController);
--- src/house/test/house.service.spec.ts
+++ src/house/test/house.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { House } from 'src/entities/house.entity';
+import { HouseMember } from 'src/entities/houseMember.entity';
+import { Repository } from 'typeorm';
 import { HouseService } from '../house.service';
 
 describe('HouseService', () => {
   let service: HouseService;
+  let houseRepository: Repository<House>;
+  let houseMemberRepository: Repository<HouseMember>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HouseService],
+      providers: [
+        HouseService,
+        {
+          provide: getRepositoryToken(House),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(HouseMember),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<HouseService>(HouseService);
