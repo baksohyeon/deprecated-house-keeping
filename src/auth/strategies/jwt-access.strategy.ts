aa16797922db41572e23f4031c2b4b9b4195a500
baksohyeon <bori12370@gmail.com>
2023-02-08 17:07:10 +0900

    refactor: 페이로드 값 대신 유저 객체 반환하도록 수정
    style: 필요없는 import문 삭제

--- src/auth/strategies/jwt-access.strategy.ts
+++ src/auth/strategies/jwt-access.strategy.ts
@@ -6,28 +6,22 @@ import {
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
-import { User } from 'src/entities/user.entity';
-import { UserService } from 'src/user/user.service';
-import { Repository } from 'typeorm';
-import { request, Request } from 'express';
-import { accessTokenCookieExtractor } from 'src/util/cookie-extractor.util';
-import { AuthService } from '../auth.service';
 import { AccessTokenPayload } from 'src/interfaces/tokens.interface';
 import { RedisService } from 'src/auth/redis/redis.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class JwtAuthStrategy extends PassportStrategy(Strategy, 'jwt-access') {
   constructor(
     readonly configService: ConfigService,
     private readonly redisService: RedisService,
+    private readonly userService: UserService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
-      issuer: 'dorito',
     });
   }
 
@@ -35,6 +29,8 @@ export class JwtAuthStrategy extends PassportStrategy(Strategy, 'jwt-access') {
 
   async validate(jwtPayload: AccessTokenPayload, done: VerifiedCallback) {
     const userId = jwtPayload.userId;
-    done(null, userId);
+    // TODO: access 토큰이 레디스에 존재하면 오류 반환
+    const user = await this.userService.findUserById(userId);
+    done(null, user);
   }
 }
