53a204ffb40109683eeecff17b56b6224905f5f3
baksohyeon <bori12370@gmail.com>
2023-02-17 16:38:43 +0900

    style: 필요없는 import 삭제

--- src/house/house.module.ts
+++ src/house/house.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { House } from 'src/entities/house.entity';
 import { HouseMember } from 'src/entities/houseMember.entity';
-import { UserModule } from 'src/user/user.module';
-import { UserService } from 'src/user/user.service';
 import { HouseController } from './house.controller';
 import { HouseService } from './house.service';
 
