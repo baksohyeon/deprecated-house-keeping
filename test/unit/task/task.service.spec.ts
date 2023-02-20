dd72c1372c4f3044846e9e23e9ba9b3527d19d7a
baksohyeon <bori12370@gmail.com>
2023-02-20 17:52:03 +0900

    test: task 테스트 모듈 셋업

--- test/unit/task/task.service.spec.ts
+++ test/unit/task/task.service.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Housework } from 'src/entities/housework.entity';
+import { Task } from 'src/entities/task.entity';
+import { User } from 'src/entities/user.entity';
 import { TaskService } from '../../../src/task/task.service';
 
 describe('TaskService', () => {
@@ -6,7 +10,19 @@ describe('TaskService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      providers: [
+        TaskService,
+        {
+          provide: getRepositoryToken(Task),
+          useValue: {},
+        },
+        { provide: getRepositoryToken(Housework), useValue: {} },
+
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<TaskService>(TaskService);
