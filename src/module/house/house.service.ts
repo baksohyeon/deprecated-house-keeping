28d09564be1a663e0058dc0004d79763e94a8e9f
baksohyeon <bori12370@gmail.com>
2023-02-21 19:04:24 +0900

    refactor: 트랜잭션 추가

--- src/module/house/house.service.ts
+++ src/module/house/house.service.ts
@@ -5,7 +5,7 @@ import { House } from 'src/entities/house.entity';
 import { HouseMember } from 'src/entities/houseMember.entity';
 import { User } from 'src/entities/user.entity';
 import { UserService } from 'src/module/user/user.service';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { UpdateHouseDto } from './dto/update-house.dto';
 import { SoftDeleteQueryBuilder } from 'typeorm/query-builder/SoftDeleteQueryBuilder';
 import { CreateHouseworkDto } from '../housework/dto/createHousework.dto';
@@ -19,24 +19,26 @@ export class HouseService {
     private readonly houseRepository: Repository<House>,
     @InjectRepository(HouseMember)
     private readonly houseMemberRepository: Repository<HouseMember>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async createNewHouse(
     createHouseDto: CreateHouseDto,
     user: User,
   ): Promise<HouseMember> {
-    // TODO: 트랜잭션 붙이기
-    const houseEntity = this.houseRepository.create({
-      name: createHouseDto.name,
+    return this.dataSource.transaction(async (manager) => {
+      const houseEntity = this.houseRepository.create({
+        name: createHouseDto.name,
+      });
+      const house = await manager.save(houseEntity);
+      const houseMemberEntity = new HouseMember();
+      Object.assign(houseMemberEntity, {
+        house,
+        user,
+        role: Role.Admin,
+      } as Partial<HouseMember>);
+      return manager.save(houseMemberEntity);
     });
-    const house = await this.houseRepository.save(houseEntity);
-    const houseMemberEntity = new HouseMember();
-    Object.assign(houseMemberEntity, {
-      house,
-      user,
-      role: Role.Admin,
-    } as Partial<HouseMember>);
-    return this.houseMemberRepository.save(houseMemberEntity);
   }
 
   async getAllHouseByUser(user: User) {
